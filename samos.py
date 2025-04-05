510f1ca39fb5785091789363055257f1a9c380f0
arl0011 <arlenbajgazakov@gmail.com>
2025-04-05 13:01:19 +0600

    lesson

--- samos.py
+++
@@ -1,8 +0,0 @@
-def get_name_and_age(name: str, birth_year: int):
-    age = 2025 - birth_year
-    return f"{name}, {age} лет"
-
-name_and_age = get_name_and_age("Арлен",2000)
-print(name_and_age)
-print(get_name_and_age("Нурадил",1990))
-
